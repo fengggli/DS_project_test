d10a3b6556dc3dd6ea76f3ec891bac4106c9e325
lifenggg <lf921227@gmail.com>
2016-11-27 14:56:32 -0500

    now I can get the formatted result

--- run_summarize.sh
+++ run_summarize.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
-SUMMARY_PATH=summary.txt
+# create the results directory
+RESULT_PATH=results
+mkdir -pv ${RESULT_PATH}
+
+SUMMARY_PATH=${RESULT_PATH}/summary.txt
 rm -rf ${SUMMARY_PATH}
 
 SLEEP_TIME=0.5
-for NUM_PROCESS in '100'
+for NUM_PROCESS in 50 100
 do
-TIME_PATH=time_${NUM_PROCESS}.txt
-rm -rf ${TIME_PATH}
-
+TIME_PATH=${RESULT_PATH}/time_${NUM_PROCESS}.txt
+rm -f ${TIME_PATH}
     for i in `seq 1 $NUM_PROCESS`;
     do
         # collect timing of each user
@@ -15,5 +18,5 @@ rm -rf ${TIME_PATH}
     done
 
     # write to the summary
-    awk '{sum+=$1}END{printf "N_process='$NUM_PROCESS'\tSum=%d\tCount=%d\tAve=%.2f\n",sum,NR,sum/NR}' ${TIME_PATH} >> ${SUMMARY_PATH}
+    awk '{sum+=$1}END{printf "N_process=%d\tSum=%d\tCount=%d\tAvg=%.2f\n",'$NUM_PROCESS',sum,NR,sum/NR}' ${TIME_PATH} >> ${SUMMARY_PATH}
 done
