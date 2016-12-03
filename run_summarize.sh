5edb63255aead49564fa91f7ec580b49351ac888
lifenggg <lf921227@gmail.com>
2016-12-03 15:03:31 -0500

    this is tested in my laptop

--- run_summarize.sh
+++ run_summarize.sh
@@ -7,10 +7,11 @@ SUMMARY_PATH=${RESULT_PATH}/summary.txt
 rm -rf ${SUMMARY_PATH}
 
 SLEEP_TIME=0.5
-for NUM_PROCESS in 50 100
+for NUM_PROCESS in 10 20 50 100 150 200
 do
-TIME_PATH=${RESULT_PATH}/time_${NUM_PROCESS}.txt
-rm -f ${TIME_PATH}
+    echo 'process requests = '$NUM_PROCESS''
+    TIME_PATH=${RESULT_PATH}/time_${NUM_PROCESS}.txt
+    rm -f ${TIME_PATH}
     for i in `seq 1 $NUM_PROCESS`;
     do
         # collect timing of each user
@@ -18,5 +19,6 @@ rm -f ${TIME_PATH}
     done
 
     # write to the summary
-    awk '{sum+=$1}END{printf "N_process=%d\tSum=%d\tCount=%d\tAvg=%.2f\n",'$NUM_PROCESS',sum,NR,sum/NR}' ${TIME_PATH} >> ${SUMMARY_PATH}
+    #awk '{sum+=$1}END{printf "N_process=%d\tSum=%d\tCount=%d\tAvg=%.2f\n",'$NUM_PROCESS',sum,NR,sum/NR}' ${TIME_PATH} >> ${SUMMARY_PATH}
+    awk '{sum+=$1}END{printf "N_process=%d\tAvg=%.2f ms\n",'$NUM_PROCESS',sum/NR}' ${TIME_PATH} >> ${SUMMARY_PATH}
 done
--- run_test.sh
+++ run_test.sh
@@ -7,7 +7,7 @@ NUM_PROCESS=200
 for i in `seq 1 ${NUM_PROCESS}`;
 do
 	# firefox -new-tab http://localhost:8080/FiveInARow/jsp/runtest.jsp &
-    phantomjs run-jasmine.js http://${SERVER_HOST}:8080/FiveInARow/jsp/runtest.jsp > logs/${NUM_PROCESS}_${i}_s_${SLEEP_TIME}.log &
+    phantomjs run_game.js http://${SERVER_HOST}:8080/FiveInARow/jsp/runtest.jsp > logs/${NUM_PROCESS}_${i}_s_${SLEEP_TIME}.log &
 	sleep ${SLEEP_TIME}
     echo "user ${i} of ${NUM_PROCESS} started"
 done
